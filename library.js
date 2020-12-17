0ce7354bafd5b3b576bee50ab60d3b35c46cfe12
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2020-12-17 21:13:12 +0200

    book form works

--- library.js
+++ library.js
@@ -3,8 +3,10 @@ let gridContainer = document.querySelector('.grid-container');
 
 let form = document.querySelector('form');
 
-let bookForm = document.querySelector('.my-form')
-let addBookButton = document.querySelector('#add-book')
+let addBookButton = document.querySelector('#add-book-btn');
+let readingStatusBtn = document.querySelector('.reading-status-btn');
+
+let bookFormBtn = document.querySelector('.add-book');
 
 let library = [];
 
@@ -18,19 +20,11 @@ class Book {
     }
 }
 
-let hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 'Fantasy adventure', 295, false);
-let hungerGames = new Book('Hunger Games', 'Stacey Mellody', 'Dystopia', 465, true);
-let vagabond = new Book('Vagabonding', 'Rolf Potts', 'Travel', 241, true);
-
-function addBookToLibrary() {
-    library.push(hobbit);
-    library.push(hungerGames);
-    library.push(vagabond);
-
+function addBookToLibrary(book) {
+    library.push(book);
 }
 
-function displayBook() {
-
+function displayBooks() {
 
     for (let i = 0; i < library.length; i++) {
         let bookObj = library[i];
@@ -54,10 +48,13 @@ function displayBook() {
                     bookCard.innerHTML += "<div><p> " + bookObj[key] + " pages</p></div>";
                     break;
                 case 'readingStatus':
-                    if (bookObj[key]) {
-                        bookCard.innerHTML += "<div><p>Read &#9989; </p></div>";
+                    console.log(bookObj[key]);
+                    if (bookObj[key] == 'read') {
+                        bookCard.innerHTML += "<div><button class='reading-status-btn'>Read</button></div>";                        
+                    } else if(bookObj[key] == 'not-read') {
+                        bookCard.innerHTML += "<div><button class='reading-status-btn not-read-btn'>Not read</button></div>"; 
                     } else {
-                        bookCard.innerHTML += "<div><p>Read &#10060;</p></div>";
+                        bookCard.innerHTML += "<div><button class='reading-status-btn in-progress-btn'>In progress</button></div>";
                     }
                     break;
             }
@@ -65,34 +62,50 @@ function displayBook() {
     }
 }
 
-let bookTitle;
-addBookButton.addEventListener('click', function () {
-    getNewBookObject();
-})
+function clearView() {
+    gridContainer.innerHTML = '';
+}
 
 function getNewBookObject() {
-
     let newBook = [];
 
     for (let i = 0; i < form.length; i++) {
         let formElement = form.elements[i];
-        newBook.push(formElement.value);
+        if(formElement.name == 'title' || formElement.name == 'author' || formElement.name == 'genre' || formElement.name == 'pages' || (formElement.name == 'readingStatus' && formElement.checked)) {
+            newBook.push(formElement.value);
+        }        
     }
-
+    
+    newBook = new Book(newBook[0], newBook[1], newBook[2], newBook[3], newBook[4] );
+    addBookToLibrary(newBook);
+    clearView();
+    displayBooks();
 
 console.log(newBook);
+}
+
+function changeReadingStatus(e) {
+    readingStatusBtn.classList.toggle('not-read-btn');  
 }
 
 
 
 
 
+addBookButton.addEventListener('click', function () {
+    getNewBookObject();
+})
+
+readingStatusBtn.addEventListener('click', function(e) {
+    changeReadingStatus(e);
+})
+
+bookFormBtn.addEventListener('click', function() {
+    form.style.display = 'block';
+})
 
 
 
 
 
 
-
-addBookToLibrary();
-displayBook();
