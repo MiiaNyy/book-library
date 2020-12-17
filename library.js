d7f1067034fd0aee882d8d14b033c97142bcd282
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2020-12-17 17:46:19 +0200

    form added

--- library.js
+++ library.js
@@ -1,29 +1,26 @@
 //let bookCard = document.querySelector('.book-card');
 let gridContainer = document.querySelector('.grid-container');
+
+let form = document.querySelector('form');
+
+let bookForm = document.querySelector('.my-form')
+let addBookButton = document.querySelector('#add-book')
+
 let library = [];
 
 class Book {
-    constructor(title, author, pages, readingStatus) {
+    constructor(title, author, genre, pages, readingStatus) {
         this.title = title;
         this.author = author;
+        this.genre = genre;
         this.pages = pages;
         this.readingStatus = readingStatus;
     }
-    info() {
-        if (this.readingStatus) {
-            this.readingStatus = 'read it.';
-        } else {
-            this.readingStatus = 'not read it yet.';
-        }
-
-        return this.title + ' by ' + this.author + ' is ' + this.pages + ' long. I have ' + this.readingStatus;
-
-    }
 }
 
-let hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-let hungerGames = new Book('Hunger Games', 'Stacey Mellody', 465, true);
-let vagabond = new Book('The Vagabond', 'James Potts', 241, true);
+let hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 'Fantasy adventure', 295, false);
+let hungerGames = new Book('Hunger Games', 'Stacey Mellody', 'Dystopia', 465, true);
+let vagabond = new Book('Vagabonding', 'Rolf Potts', 'Travel', 241, true);
 
 function addBookToLibrary() {
     library.push(hobbit);
@@ -45,26 +42,55 @@ function displayBook() {
         for (let key in bookObj) {
             switch (key) {
                 case 'title':
-                    bookCard.innerHTML += "<h4>" + bookObj[key] + "</h4>";
+                    bookCard.innerHTML += "<div><h4>" + bookObj[key] + "</h4></div>";
                     break;
                 case 'author':
-                    bookCard.innerHTML += "<p>Author: " + bookObj[key] + "</p>";
+                    bookCard.innerHTML += "<div><p>Author:<br> " + bookObj[key] + "</p></div>";
+                    break;
+                case 'genre':
+                    bookCard.innerHTML += "<div><p> Genre:<br> " + bookObj[key] + "</p></div>";
                     break;
                 case 'pages':
-                    bookCard.innerHTML += "<p> " + bookObj[key] + " pages</p>";
+                    bookCard.innerHTML += "<div><p> " + bookObj[key] + " pages</p></div>";
                     break;
                 case 'readingStatus':
-                    if(bookObj[key]) {
-                        bookCard.innerHTML += "<p>Read &#9989; </p>";
+                    if (bookObj[key]) {
+                        bookCard.innerHTML += "<div><p>Read &#9989; </p></div>";
                     } else {
-                        bookCard.innerHTML += "<p>Read &#10060;</p>";
+                        bookCard.innerHTML += "<div><p>Read &#10060;</p></div>";
                     }
                     break;
-                    }
-
             }
         }
     }
+}
+
+let bookTitle;
+addBookButton.addEventListener('click', function () {
+    getNewBookObject();
+})
+
+function getNewBookObject() {
+
+    let newBook = [];
+
+    for (let i = 0; i < form.length; i++) {
+        let formElement = form.elements[i];
+        newBook.push(formElement.value);
+    }
+
+
+console.log(newBook);
+}
+
+
+
+
+
+
+
+
+
 
 
 
