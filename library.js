a40bf130d4786403892d3de8e99b0744a4f11e13
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2020-12-18 17:23:18 +0200

    close window function added

--- library.js
+++ library.js
@@ -7,6 +7,8 @@ let addBookButton = document.querySelector('#add-book-btn');
 let readingStatusBtn = document.querySelector('.reading-status-btn');
 
 let bookFormBtn = document.querySelector('.add-book');
+let closeWindowBtn = document.querySelector('.close-window');
+
 
 let library = [];
 
@@ -24,41 +26,76 @@ function addBookToLibrary(book) {
     library.push(book);
 }
 
+function checkIfEmpty() {
+    const input = document.querySelectorAll('input');
+
+    input.forEach(element => {
+        element.addEventListener('input', evt => {
+            const value = element.value;
+            console.log(element.value)
+            if (!value) {
+                element.dataset.state = ''
+                return
+            }
+            
+
+            const trimmed = value.trim()
+
+            if (trimmed) {
+                element.dataset.state = 'valid';
+            } else {
+                element.dataset.state = 'invalid';
+            }
+        })
+    });
+
+
+
+}
+
+
+function addBookToView(bookObj) {
+    let bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+    gridContainer.appendChild(bookCard);
+
+    for (let key in bookObj) {
+        checkIfEmpty(bookObj[key]);
+        console.log(key);
+        switch (key) {
+            case 'title':
+                bookCard.innerHTML += "<div><h4>" + bookObj[key] + "</h4></div>";
+                break;
+            case 'author':
+                bookCard.innerHTML += "<div><p>Author:<br> " + bookObj[key] + "</p></div>";
+                break;
+            case 'genre':
+                bookCard.innerHTML += "<div><p> Genre:<br> " + bookObj[key] + "</p></div>";
+                break;
+            case 'pages':
+                bookCard.innerHTML += "<div><p> " + bookObj[key] + " pages</p></div>";
+                break;
+            case 'readingStatus':
+                console.log(bookObj[key]);
+                if (bookObj[key] == 'read') {
+                    bookCard.innerHTML += "<div><button class='reading-status-btn'>Read</button></div>";
+                } else if (bookObj[key] == 'not-read') {
+                    bookCard.innerHTML += "<div><button class='reading-status-btn not-read-btn'>Not read</button></div>";
+                } else {
+                    bookCard.innerHTML += "<div><button class='reading-status-btn in-progress-btn'>In progress</button></div>";
+                }
+                break;
+            case ' ':
+                console.log(bookObj + ' is empty');
+                break;
+        }
+    }
+}
+
 function displayBooks() {
 
     for (let i = 0; i < library.length; i++) {
-        let bookObj = library[i];
-
-        let bookCard = document.createElement('div');
-        bookCard.classList.add('book-card');
-        gridContainer.appendChild(bookCard);
-
-        for (let key in bookObj) {
-            switch (key) {
-                case 'title':
-                    bookCard.innerHTML += "<div><h4>" + bookObj[key] + "</h4></div>";
-                    break;
-                case 'author':
-                    bookCard.innerHTML += "<div><p>Author:<br> " + bookObj[key] + "</p></div>";
-                    break;
-                case 'genre':
-                    bookCard.innerHTML += "<div><p> Genre:<br> " + bookObj[key] + "</p></div>";
-                    break;
-                case 'pages':
-                    bookCard.innerHTML += "<div><p> " + bookObj[key] + " pages</p></div>";
-                    break;
-                case 'readingStatus':
-                    console.log(bookObj[key]);
-                    if (bookObj[key] == 'read') {
-                        bookCard.innerHTML += "<div><button class='reading-status-btn'>Read</button></div>";                        
-                    } else if(bookObj[key] == 'not-read') {
-                        bookCard.innerHTML += "<div><button class='reading-status-btn not-read-btn'>Not read</button></div>"; 
-                    } else {
-                        bookCard.innerHTML += "<div><button class='reading-status-btn in-progress-btn'>In progress</button></div>";
-                    }
-                    break;
-            }
-        }
+        addBookToView(library[i]);
     }
 }
 
@@ -71,41 +108,48 @@ function getNewBookObject() {
 
     for (let i = 0; i < form.length; i++) {
         let formElement = form.elements[i];
-        if(formElement.name == 'title' || formElement.name == 'author' || formElement.name == 'genre' || formElement.name == 'pages' || (formElement.name == 'readingStatus' && formElement.checked)) {
+        if (formElement.name == 'title' || formElement.name == 'author' || formElement.name == 'genre' || formElement.name == 'pages' || (formElement.name == 'readingStatus' && formElement.checked)) {
             newBook.push(formElement.value);
-        }        
+        }
     }
-    
-    newBook = new Book(newBook[0], newBook[1], newBook[2], newBook[3], newBook[4] );
+
+    newBook = new Book(newBook[0], newBook[1], newBook[2], newBook[3], newBook[4]);
     addBookToLibrary(newBook);
-    clearView();
-    displayBooks();
+    addBookToView(newBook);
 
-console.log(newBook);
+
+    console.log(Object.values(newBook));
+
+
 }
 
-function changeReadingStatus(e) {
-    readingStatusBtn.classList.toggle('not-read-btn');  
+
+function toggleFormVisibility() {
+    if (form.style.display === "none") {
+        form.style.display = "block";
+
+    } else {
+        form.style.display = "none";
+    }
 }
 
-
-
-
+closeWindowBtn.addEventListener('click', function() {
+    form.style.display = 'none';
+})
 
 addBookButton.addEventListener('click', function () {
     getNewBookObject();
+
 })
 
-readingStatusBtn.addEventListener('click', function(e) {
+readingStatusBtn.addEventListener('click', function (e) {
     changeReadingStatus(e);
 })
 
-bookFormBtn.addEventListener('click', function() {
-    form.style.display = 'block';
+bookFormBtn.addEventListener('click', function () {
+    toggleFormVisibility();
+    form.classList.toggle('alert')
+
 })
 
-
-
-
-
-
+checkIfEmpty();
